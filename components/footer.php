b86e1bf5f3ed73a77a0b7c2f0dfa5055d2bd66ba
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-06-04 21:53:15 -0300

    inserindo componente footer.php e estruturação e dando inicio a estilizações

---
+++ components/footer.php
@@ -0,0 +1,62 @@
+<div class="top-footer">
+    <h2><i class="bx bx-envelope"></i>Entrar para Newsletter</h2>
+    <div class="input-field">
+        <input type="text" name="" placeholder="Endereço de email..">
+        <button class="btn">Increver-se</button>
+    </div>
+</div>
+<footer>
+    <div class="overlay"></div>
+    <div class="footer-content">
+        <div class="img-box">
+            <img src="img/logo2.png" alt="">
+        </div>
+        <div class="inner-footer">
+            <div class="card">
+                <h3>Sobre nós</h3>
+                <ul>
+                    <li>Sobre nós</li>
+                    <li>Diferenciais</li>
+                    <li>Comunidade</li>
+                    <li>Imprensa</li>
+                    <li>Blog</li>
+                    <li>Vídeos</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h3>Serviços</h3>
+                <ul>
+                    <li>Pedidos</li>
+                    <li>Central de ajuda</li>
+                    <li>Entrega</li>
+                    <li>Termos de uso</li>
+                    <li>Detalhes da conta</li>
+                    <li>Minha conta</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h3>Localização</h3>
+                <ul>
+                    <li>Curitiba</li>
+                    <li>Paraná</li>
+                    <li>Campo Comprido</li>
+                    <li>Brasil</li>                    
+                </ul>
+            </div>
+            <div class="card">
+                <h3>Newsletter</h3>
+                <p>Entrar em Newsletter</p>
+                <div class="social-links">
+                    <i class="bx bxl-instagram"></i>
+                    <i class="bx bxl-twitter"></i>
+                    <i class="bx bxl-facebook"></i>
+                    <i class="bx bxl-instagram"></i>
+                    <i class="bx bxl-youtube"></i>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <p>All rights reserved - Bera Tech Sistemas</p>
+        </div>
+    </div>
+</footer>
