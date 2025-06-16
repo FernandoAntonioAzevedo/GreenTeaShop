cce40030869a298b923f0272f657256ad77dd64d
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-06-16 18:25:28 -0300

    avançando em inserções de sections em home.php

--- home.php
+++ home.php
@@ -83,8 +83,44 @@
           <div class="box-container">
               <div class="box">
                   <img src="img/thumb2.jpg" alt="">
-                  <h3>green tea</h3>  
-              </div>  
+                  <h3>green tea</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <i class="bx bx-chevron-right"></i>  
+              </div>
+              
+              <div class="box">
+                  <img src="img/thumb0.jpg" alt="">
+                  <h3>lemon tea</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <i class="bx bx-chevron-right"></i>  
+              </div>
+
+              <div class="box">
+                  <img src="img/thumb1.jpg" alt="">
+                  <h3>green coffe</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <i class="bx bx-chevron-right"></i>  
+              </div>
+
+              <div class="box">
+                  <img src="img/thumb.jpg" alt="">
+                  <h3>green tea</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <i class="bx bx-chevron-right"></i>  
+              </div>
+          </div>  
+      </section>
+      <section class="container">
+          <div class="box-container">
+               <div class="box">
+                   <img src="img/about-us.jpg" alt=""> 
+               </div>
+               <div class="box">
+                   <img src="img/download.png" alt=""> 
+                   <span>Healthy tea</span>
+                   <h1>save up tp 50% off</h1>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iste qui dolorum?</p>
+               </div> 
           </div>  
       </section> 
       <?php include 'components/footer.php'; ?>        
