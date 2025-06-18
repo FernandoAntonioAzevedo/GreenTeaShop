a424f872f9e9f5ed53c7e5b2d2cb6d6e5a9bbe88
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-06-17 23:08:46 -0300

    finalizando a home e dando inicio a page about.php

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php  
+    include 'components/connection.php';
+?>
+
+<style type="text/css">
+    <?php include 'css/styles.css'; ?>
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale= 1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Green Tea - Shop</title>
+</head>
+<body>
+    <?php include 'components/header.php'; ?>
+    <div class="main">        
+        
+       
+      <?php include 'components/footer.php'; ?>        
+    </div>
+    <script src="https://cdnjs.cloudfare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="js/script.js"></script>
+    <?php include 'components/alert.php'; ?>
+</body>
+</html>    
--- home.php
+++ home.php
@@ -112,7 +112,7 @@
       </section>
       <section class="container">
           <div class="box-container">
-               <div class="box">
+              <div class="box box-about">
                    <img src="img/about-us.jpg" alt=""> 
                </div>
                <div class="box">
@@ -142,29 +142,100 @@
         <div class="box-container">
             <div class="box">
                 <img src="img/card.jpg" alt="">
-                <a href="view_products.php">Comprar agora</a>
+                <a href="view_products.php" class="btn">Comprar agora</a>
             </div>
             <div class="box">
                 <img src="img/card0.jpg" alt="">
-                <a href="view_products.php">Comprar agora</a>
+                <a href="view_products.php" class="btn">Comprar agora</a>
             </div>
             <div class="box">
                 <img src="img/card1.jpg" alt="">
-                <a href="view_products.php">Comprar agora</a>
+                <a href="view_products.php" class="btn">Comprar agora</a>
             </div>
             <div class="box">
                 <img src="img/card2.jpg" alt="">
-                <a href="view_products.php">Comprar agora</a>
+                <a href="view_products.php" class="btn">Comprar agora</a>
             </div>
             <div class="box">
                 <img src="img/10.jpg" alt="">
-                <a href="view_products.php">Comprar agora</a>
+                <a href="view_products.php" class="btn">Comprar agora</a>
             </div>
             <div class="box">
                 <img src="img/6.webp" alt="">
-                <a href="view_products.php">Comprar agora</a>
+                <a href="view_products.php" class="btn">Comprar agora</a>
             </div>
         </div>
+      </section>
+      <section class="shop-category">
+         <div class="box-container">
+             <div class="box">
+                <img src="img/6.jpg" alt="">
+                <div class="detail">
+                    <span>BIG OFFERS</span>
+                    <h1>Extra 15% off</h1>
+                    <a href="view_products.php" class="btn">Comprar agora</a>
+                </div>
+             </div>
+             <div class="box">
+                <img src="img/7.jpg" alt="">
+                <div class="detail">
+                    <span>new in taste</span>
+                    <h1>coffee house</h1>
+                    <a href="view_products.php" class="btn">Comprar agora</a>
+                </div>
+             </div>
+         </div>
+      </section>
+      <section class="services">
+          <div class="box-container">
+              <div class="box">
+                  <img src="img/icon2.png" alt="">
+                  <div class="detail">
+                      <h3>great savings</h3>
+                      <p>save big every order</p>  
+                  </div>  
+              </div>
+              <div class="box">
+                  <img src="img/icon1.png" alt="">
+                  <div class="detail">
+                      <h3>24h Support</h3>
+                      <p>one-on-one support</p>  
+                  </div>  
+              </div>
+              <div class="box">
+                  <img src="img/icon0.png" alt="">
+                  <div class="detail">
+                      <h3>gift vouchers</h3>
+                      <p>vouchers on every festivals</p>  
+                  </div>  
+              </div>
+              <div class="box">
+                  <img src="img/icon.png" alt="">
+                  <div class="detail">
+                      <h3>worldwide delivery</h3>
+                      <p>dropship worldwide</p>  
+                  </div>  
+              </div>  
+          </div>
+      </section>
+      <section class="brand">
+          <div class="box-container">
+              <div class="box">
+                 <img src="img/brand (1).jpg" alt="">
+              </div>
+              <div class="box">
+                 <img src="img/brand (2).jpg" alt="">
+              </div>
+              <div class="box">
+                 <img src="img/brand (3).jpg" alt="">
+              </div>
+              <div class="box">
+                 <img src="img/brand (4).jpg" alt="">
+              </div>
+              <div class="box">
+                 <img src="img/brand (5).jpg" alt="">
+              </div>  
+          </div>  
       </section> 
       <?php include 'components/footer.php'; ?>        
     </div>
